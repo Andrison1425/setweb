a5971b1f4309f9bd8e01518f5ba9e4fafd0a607b
Andrison Sánchez <cuentasecundariadeandrison@gmail.com>
2021-02-18 21:06:39 -0300

    Añadiendo a Git

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+$(function(){
+
+    jQuery(window).scroll(function () {
+        if (jQuery(window).scrollTop() > 400) {
+            jQuery("#navigation").css("background-color","#00C7FC");
+            jQuery("#navigation").addClass("animated-nav");
+        } else {
+            jQuery("#navigation").css("background-color","transparent");
+            jQuery("#navigation").removeClass("animated-nav");
+        }
+    });
+
+    $('#nav').onePageNav({
+        filter: ':not(.external)',
+        scrollSpeed: 950,
+        scrollThreshold: 1
+    });
+
+    //fancybox
+
+    $(".fancybox").fancybox({
+        padding: 0,
+
+        openEffect : 'elastic',
+        openSpeed  : 650,
+
+        closeEffect : 'elastic',
+        closeSpeed  : 550,
+    });
+
+});
+
+/* ========================================================================= */
+/*  On scroll fade/bounce fffect
+/* ========================================================================= */
+
+    wow = new WOW({
+        animateClass: 'animated',
+        offset: 100,
+        mobile: false
+    });
+    wow.init();
